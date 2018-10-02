f738522b4a30a6d92707583b55dea46810552ee4
gberthou <gautier.berthou@outlook.fr>
2018-10-02 23:06:47 +0200

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -18,12 +18,6 @@ const size_t WIDTH = 9;
 const size_t HEIGHT = 11;
 static Dictionary dictionary("dict", HEIGHT);
 
-static DFA * dfa_borderH;
-static DFA * dfa_borderV;
-static DFA * dfa_firstH;
-static DFA * dfa_firstV;
-static DFA * dfa_secondH;
-static DFA * dfa_secondV;
 static DFA * dfa_noindex;
 
 static std::mutex cout_mutex;
@@ -46,90 +40,10 @@ class Crosswords: public Script
             height(height),
 
             letters(*this, width * height, 'a', 'z'+1) // Letters go from 'a' to 'z', and black tile is 'z'+1 ('{')
-
-            /*
-            indBH(*this, 2, dictionary.FirstIndexOfLength(width), dictionary.LastIndexOfLength(width)),
-            indBV(*this, 2, dictionary.FirstIndexOfLength(height), dictionary.LastIndexOfLength(height)),
-
-            ind1H(*this, height-2, dictionary.FirstIndexOfLength(2), dictionary.LastIndexOfLength(width)),
-            ind2H(*this, height-2, MIN_INDEX, dictionary.LastIndexOfLength(width-3)),
-            ind1V(*this, width-2, dictionary.FirstIndexOfLength(2), dictionary.LastIndexOfLength(height)),
-            ind2V(*this, width-2, MIN_INDEX, dictionary.LastIndexOfLength(height-3)),
-            
-            wordPos1H(*this, height-2, 0, 2),
-            wordPos2H(*this, height-2, 3, width+1),
-            wordPos1V(*this, width-2, 0, 2),
-            wordPos2V(*this, width-2, 3, height+1),
-
-            wordLen1H(*this, height-2, 2, width),
-            wordLen1V(*this, width-2, 2, height)
-            */
         {
             // Fewer black tiles than X
             count(*this, letters, 'z'+1, IRT_LQ, 10); // <= 10
 
-#if 0
-            auto allIndices = indBH+indBV+ind1H+ind2H+ind1V+ind2V;
-
-            distinct(*this, allIndices, MIN_INDEX);
-
-            /* Borders */
-            // First row
-            IntVarArgs topborderH = letters.slice(0, 1, width) + indBH[0];
-            extensional(*this, topborderH, *dfa_borderH);
-
-            // Last row
-            IntVarArgs botborderH = letters.slice((height-1)*width, 1, width) + indBH[1];
-            extensional(*this, botborderH, *dfa_borderH);
-
-            // First column
-            IntVarArgs leftborderV = letters.slice(0, width, height) + indBV[0];
-            extensional(*this, leftborderV, *dfa_borderV);
-            
-            // Last column
-            IntVarArgs rightborderV = letters.slice(width-1, width, height) + indBV[1];
-            extensional(*this, rightborderV, *dfa_borderV);
-
-            // Impose mandatory words
-            for(size_t i = 0; i < orderedMandatory.size(); ++i)
-            {
-                int index = orderedMandatory[i];
-                if(index)
-                    rel(*this, allIndices[i], IRT_EQ, index);
-            }
-
-            // Horizontal words
-            for(size_t y = 1; y < height-1; ++y)
-            {
-                // First words
-                IntVarArgs row = letters.slice(y*width, 1, width);
-                extensional(*this, wordPos1H[y-1] + row + ind1H[y-1] + wordLen1H[y-1], *dfa_firstH);
-
-                // Second words
-                IntVarArgs reducedRow = letters.slice(y*width+3, 1, width-3);
-                extensional(*this, wordPos2H[y-1] + reducedRow + ind2H[y-1], *dfa_secondH);
-                rel(*this, wordPos2H[y-1] == wordPos1H[y-1] + wordLen1H[y-1] + 1);
-            }
-            
-            // Vertical words
-            for(size_t x = 1; x < width-1; ++x)
-            {
-                // First words
-                IntVarArgs col = letters.slice(x, width, height);
-                extensional(*this, wordPos1V[x-1] + col + ind1V[x-1] + wordLen1V[x-1], *dfa_firstV);
-
-                // Second words
-                IntVarArgs reducedCol = letters.slice(x+3*width, width, height-3);
-                extensional(*this, wordPos2V[x-1] + reducedCol + ind2V[x-1], *dfa_secondV);
-                rel(*this, wordPos2V[x-1] == wordPos1V[x-1] + wordLen1V[x-1] + 1);
-            }
-
-            auto seed = std::time(nullptr);
-            branch(*this, indBH+indBV+ind1H+ind1V, INT_VAR_SIZE_MIN(), INT_VAL_RND(seed));
-            branch(*this, ind2H+ind2V, INT_VAR_NONE(), INT_VAL_RND(seed));
-
-            branch(*this, wordPos1H+wordPos1V, INT_VAR_NONE(), INT_VAL_MIN());
-#else
             IntVarArgs allwords;
             IntVar dummy(*this, 'z'+1, 'z'+1);
             for(size_t y = 0; y < height; ++y)
@@ -155,7 +69,6 @@ class Crosswords: public Script
 
             auto seed = std::time(nullptr);
             branch(*this, letters, INT_VAR_RND(seed), INT_VAL_RND(seed));
-#endif
         }
 
         Crosswords(Crosswords &crosswords):
@@ -164,24 +77,6 @@ class Crosswords: public Script
             height(crosswords.height)
         {
             letters.update(*this, crosswords.letters);
-
-            /*
-            indBH.update(*this, crosswords.indBH);
-            indBV.update(*this, crosswords.indBV);
-
-            ind1H.update(*this, crosswords.ind1H);
-            ind2H.update(*this, crosswords.ind2H);
-            ind1V.update(*this, crosswords.ind1V);
-            ind2V.update(*this, crosswords.ind2V);
-
-            wordPos1H.update(*this, crosswords.wordPos1H);
-            wordPos2H.update(*this, crosswords.wordPos2H);
-            wordPos1V.update(*this, crosswords.wordPos1V);
-            wordPos2V.update(*this, crosswords.wordPos2V);
-
-            wordLen1H.update(*this, crosswords.wordLen1H);
-            wordLen1V.update(*this, crosswords.wordLen1V);
-            */
         }
 
         virtual Space *copy(void)
@@ -269,24 +164,6 @@ class Crosswords: public Script
         size_t width;
         size_t height;
         IntVarArray letters;
-
-        /*
-        IntVarArray indBH;
-        IntVarArray indBV;
-
-        IntVarArray ind1H;
-        IntVarArray ind2H;
-        IntVarArray ind1V;
-        IntVarArray ind2V;
-
-        IntVarArray wordPos1H;
-        IntVarArray wordPos2H;
-        IntVarArray wordPos1V;
-        IntVarArray wordPos2V;
-
-        IntVarArray wordLen1H;
-        IntVarArray wordLen1V;
-        */
 };
 
 /*
@@ -416,12 +293,6 @@ int main(void)
     for(const auto word : mandatoryWords)
         mandatoryDFAs.push_back(DictionaryDFA::Mandatory(word));
 
-    dfa_borderH = dictDFA.BorderH();
-    dfa_borderV = dictDFA.BorderV();
-    dfa_firstH = dictDFA.FirstH();
-    dfa_firstV = dictDFA.FirstV();
-    dfa_secondH = dictDFA.SecondH();
-    dfa_secondV = dictDFA.SecondV();
     dfa_noindex = dictDFA.NoIndex();
 
     std::cout << "DFA conversion done!" << std::endl;
@@ -452,12 +323,7 @@ int main(void)
     */
     run_single(4, mandatoryDFAs);
 
-    delete dfa_borderH;
-    delete dfa_borderV;
-    delete dfa_firstH;
-    delete dfa_firstV;
-    delete dfa_secondH;
-    delete dfa_secondV;
+    delete dfa_noindex;
 
     for(auto dfa : mandatoryDFAs)
         delete dfa;
