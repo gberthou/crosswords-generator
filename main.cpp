3fc2058d9551f85378c14d31f35aaf9648d5cd4d
gberthou <gautier.berthou@outlook.fr>
2019-07-09 15:42:46 +0200

    Fancy borders

--- main.cpp
+++ main.cpp
@@ -29,52 +29,52 @@ static std::mutex cout_mutex;
 class Crosswords: public Script
 {
     public:
-        Crosswords(const SizeOptions &opt, size_t width, size_t height, const std::vector<int> &orderedMandatory = std::vector<int>()):
+        Crosswords(const SizeOptions &opt, size_t width, size_t height, bool fancyBorders, const std::vector<int> &orderedMandatory = std::vector<int>()):
             Script(opt),
             width(width),
             height(height),
 
             letters(*this, width * height, 'a', 'z'+1), // Letters go from 'a' to 'z', and black tile is 'z'+1 ('{')
 
-            indBH(*this, 2, dictionary.FirstIndexOfLength(width), dictionary.LastIndexOfLength(width)),
-            indBV(*this, 2, dictionary.FirstIndexOfLength(height), dictionary.LastIndexOfLength(height)),
-
-            ind1H(*this, height-2, dictionary.FirstIndexOfLength(2), dictionary.LastIndexOfLength(width)),
-            ind2H(*this, height-2, MIN_INDEX, dictionary.LastIndexOfLength(width-3)),
-            ind1V(*this, width-2, dictionary.FirstIndexOfLength(2), dictionary.LastIndexOfLength(height)),
-            ind2V(*this, width-2, MIN_INDEX, dictionary.LastIndexOfLength(height-3)),
+            ind1H(*this, height, dictionary.FirstIndexOfLength(2), dictionary.LastIndexOfLength(width)),
+            ind2H(*this, height, MIN_INDEX, dictionary.LastIndexOfLength(width-3)),
+            ind1V(*this, width, dictionary.FirstIndexOfLength(2), dictionary.LastIndexOfLength(height)),
+            ind2V(*this, width, MIN_INDEX, dictionary.LastIndexOfLength(height-3)),
             
-            wordPos1H(*this, height-2, 0, 2),
-            wordPos2H(*this, height-2, 3, width+1),
-            wordPos1V(*this, width-2, 0, 2),
-            wordPos2V(*this, width-2, 3, height+1),
+            wordPos1H(*this, height, 0, 2),
+            wordPos2H(*this, height, 3, width+1),
+            wordPos1V(*this, width, 0, 2),
+            wordPos2V(*this, width, 3, height+1),
 
-            wordLen1H(*this, height-2, 2, width),
-            wordLen1V(*this, width-2, 2, height)
+            wordLen1H(*this, height, 2, width),
+            wordLen1V(*this, width, 2, height)
         {
             // Fewer black tiles than X
             count(*this, letters, 'z'+1, IRT_LQ, 10); // <= 10
 
-            auto allIndices = indBH+indBV+ind1H+ind2H+ind1V+ind2V;
+            auto allIndices = ind1H+ind2H+ind1V+ind2V;
 
             distinct(*this, allIndices, MIN_INDEX);
 
             /* Borders */
-            // First row
-            IntVarArgs topborderH = letters.slice(0, 1, width) + indBH[0];
-            extensional(*this, topborderH, *dfa_borderH);
+            if(fancyBorders)
+            {
+                // First row
+                rel(*this, wordPos1H[0], IRT_EQ, 0);
+                rel(*this, wordLen1H[0], IRT_EQ, width);
 
-            // Last row
-            IntVarArgs botborderH = letters.slice((height-1)*width, 1, width) + indBH[1];
-            extensional(*this, botborderH, *dfa_borderH);
+                // Last row
+                rel(*this, wordPos1H[height-1], IRT_EQ, 0);
+                rel(*this, wordLen1H[height-1], IRT_EQ, width);
 
-            // First column
-            IntVarArgs leftborderV = letters.slice(0, width, height) + indBV[0];
-            extensional(*this, leftborderV, *dfa_borderV);
-            
-            // Last column
-            IntVarArgs rightborderV = letters.slice(width-1, width, height) + indBV[1];
-            extensional(*this, rightborderV, *dfa_borderV);
+                // First column
+                rel(*this, wordPos1V[0], IRT_EQ, 0);
+                rel(*this, wordLen1V[0], IRT_EQ, height);
+
+                // Last column
+                rel(*this, wordPos1V[width-1], IRT_EQ, 0);
+                rel(*this, wordLen1V[width-1], IRT_EQ, height);
+            }
 
             // Impose mandatory words
             for(size_t i = 0; i < orderedMandatory.size(); ++i)
@@ -85,39 +85,39 @@ class Crosswords: public Script
             }
 
             // Horizontal words
-            for(size_t y = 1; y < height-1; ++y)
+            for(size_t y = 0; y < height; ++y)
             {
                 // First words
                 IntVarArgs row = letters.slice(y*width, 1, width);
-                extensional(*this, wordPos1H[y-1] + row + ind1H[y-1] + wordLen1H[y-1], *dfa_firstH);
+                extensional(*this, wordPos1H[y] + row + ind1H[y] + wordLen1H[y], *dfa_firstH);
 
                 // Second words
                 IntVarArgs reducedRow = letters.slice(y*width+3, 1, width-3);
-                extensional(*this, wordPos2H[y-1] + reducedRow + ind2H[y-1], *dfa_secondH);
+                extensional(*this, wordPos2H[y] + reducedRow + ind2H[y], *dfa_secondH);
 
-                // wp2H[y-1] == wp1H[y-1] + wl1H[y-1] + 1
-                // wp2H[y-1] * 1 + wp1H[y-1] * (-1) + wl1H[y-1] * (-1) == 1
-                linear(*this, std::vector<int> {1, -1, -1}, std::vector<IntVar> {wordPos2H[y-1], wordPos1H[y-1], wordLen1H[y-1]}, IRT_EQ, 1);
+                // wp2H[y] == wp1H[y] + wl1H[y] + 1
+                // wp2H[y] * 1 + wp1H[y] * (-1) + wl1H[y] * (-1) == 1
+                linear(*this, std::vector<int> {1, -1, -1}, std::vector<IntVar> {wordPos2H[y], wordPos1H[y], wordLen1H[y]}, IRT_EQ, 1);
             }
             
             // Vertical words
-            for(size_t x = 1; x < width-1; ++x)
+            for(size_t x = 0; x < width; ++x)
             {
                 // First words
                 IntVarArgs col = letters.slice(x, width, height);
-                extensional(*this, wordPos1V[x-1] + col + ind1V[x-1] + wordLen1V[x-1], *dfa_firstV);
+                extensional(*this, wordPos1V[x] + col + ind1V[x] + wordLen1V[x], *dfa_firstV);
 
                 // Second words
                 IntVarArgs reducedCol = letters.slice(x+3*width, width, height-3);
-                extensional(*this, wordPos2V[x-1] + reducedCol + ind2V[x-1], *dfa_secondV);
+                extensional(*this, wordPos2V[x] + reducedCol + ind2V[x], *dfa_secondV);
 
-                // wp2V[x-1] == wp1V[x-1] + wl1V[x-1] + 1
-                // wp2V[y-1] * 1 + wp1V[x-1] * (-1) + wl1V[x-1] * (-1) == 1
-                linear(*this, std::vector<int> {1, -1, -1}, std::vector<IntVar> {wordPos2V[x-1], wordPos1V[x-1], wordLen1V[x-1]}, IRT_EQ, 1);
+                // wp2V[x] == wp1V[x] + wl1V[x] + 1
+                // wp2V[y] * 1 + wp1V[x] * (-1) + wl1V[x] * (-1) == 1
+                linear(*this, std::vector<int> {1, -1, -1}, std::vector<IntVar> {wordPos2V[x], wordPos1V[x], wordLen1V[x]}, IRT_EQ, 1);
             }
 
             auto seed = std::time(nullptr);
-            branch(*this, indBH+indBV+ind1H+ind1V, INT_VAR_SIZE_MIN(), INT_VAL_RND(seed));
+            branch(*this, ind1H+ind1V, INT_VAR_SIZE_MIN(), INT_VAL_RND(seed));
             branch(*this, ind2H+ind2V, INT_VAR_NONE(), INT_VAL_RND(seed));
 
             branch(*this, wordPos1H+wordPos1V, INT_VAR_NONE(), INT_VAL_MIN());
@@ -129,9 +129,6 @@ class Crosswords: public Script
             height(crosswords.height)
         {
             letters.update(*this, crosswords.letters);
-
-            indBH.update(*this, crosswords.indBH);
-            indBV.update(*this, crosswords.indBV);
 
             ind1H.update(*this, crosswords.ind1H);
             ind2H.update(*this, crosswords.ind2H);
@@ -230,9 +227,6 @@ class Crosswords: public Script
         size_t height;
         IntVarArray letters;
 
-        IntVarArray indBH;
-        IntVarArray indBV;
-
         IntVarArray ind1H;
         IntVarArray ind2H;
         IntVarArray ind1V;
@@ -247,10 +241,10 @@ class Crosswords: public Script
         IntVarArray wordLen1V;
 };
 
-bool permutation_valid(size_t width, size_t height, const std::vector<int> &indices)
+bool permutation_valid(size_t width, size_t height, bool fancyBorders, const std::vector<int> &indices)
 {
-    // Assumes order: indBH(2) + indBV(2) + ind1H(H-2) + ind2H(H-2)
-    //              + ind1V(W-2) + ind2V(W-2);
+    // Assumes order: ind1H(H) + ind2H(H)
+    //              + ind1V(W) + ind2V(W);
 
     for(size_t i = 0; i < indices.size(); ++i)
     {
@@ -260,40 +254,38 @@ bool permutation_valid(size_t width, size_t height, const std::vector<int> &indi
             std::string word = dictionary.GetWord(index);
             size_t size = word.size();
 
-            if(i < 2)
+            if(fancyBorders)
             {
-                if(size != width)
+                if((i == 0 || i == height-1) && size != width)
+                    return false;
+                if((i == 2*height || i == 2*height+width-1) && size != height)
                     return false;
             }
-            else if(i < 4)
-            {
-                if(size != height)
-                    return false;
-            }
-            else if(i < 2 + height)
+
+            if(i < height) // ind1H
             {
                 if(size > width)
                     return false;
                 
-                int other = indices[i + height - 2];
+                int other = indices[i + height];
                 if(other && size+dictionary.GetWord(other).size()+1 > width)
                     return false;
             }
-            else if(i < 2*height)
+            else if(i < 2*height) // ind2H
             {
                 if(size + 3 > width)
                     return false;
             }
-            else if(i < 2*height + width - 2)
+            else if(i < 2*height + width) // ind1V
             {
                 if(size > height)
                     return false;
                 
-                int other = indices[i + width - 2];
+                int other = indices[i + width];
                 if(other && size+dictionary.GetWord(other).size()+1 > height)
                     return false;
             }
-            else // i < 2*(width+height-2)
+            else // ind2V
             {
                 if(size + 3 > height)
                     return false;
@@ -304,13 +296,13 @@ bool permutation_valid(size_t width, size_t height, const std::vector<int> &indi
     return true;
 }
 
-void run_single(size_t nthreads, std::vector<int> indices = std::vector<int>())
+void run_single(size_t nthreads, bool fancyBorders, std::vector<int> indices = std::vector<int>())
 {
 
     SizeOptions opt("Crosswords");
     opt.solutions(0);
 
-    Crosswords model(opt, WIDTH, HEIGHT, indices);
+    Crosswords model(opt, WIDTH, HEIGHT, fancyBorders, indices);
     Search::Options o;
     Search::Cutoff *c = Search::Cutoff::constant(70000);
     o.cutoff = c;
@@ -324,14 +316,14 @@ void run_single(size_t nthreads, std::vector<int> indices = std::vector<int>())
     }
 }
 
-void run_single_mandatory(const std::vector<int> &indices)
+void run_single_mandatory(bool fancyBorders, const std::vector<int> &indices)
 {
-    if(!permutation_valid(WIDTH, HEIGHT, indices))
+    if(!permutation_valid(WIDTH, HEIGHT, fancyBorders, indices))
         return;
-    run_single(1, indices);
+    run_single(1, fancyBorders, indices);
 }
 
-void run_concurrently(std::vector<int> indices, size_t nthreads, size_t id)
+void run_concurrently(bool fancyBorders, std::vector<int> indices, size_t nthreads, size_t id)
 {
     size_t i = 0;
     do
@@ -342,7 +334,7 @@ void run_concurrently(std::vector<int> indices, size_t nthreads, size_t id)
             continue;
         }
 
-        run_single_mandatory(indices);
+        run_single_mandatory(fancyBorders, indices);
         ++i;
     } while(std::prev_permutation(indices.begin(), indices.end()));
 }
@@ -360,6 +352,8 @@ size_t permutation_count(size_t n, size_t k)
 
 int main(void)
 {
+    const bool FANCY_BORDERS = false;
+
     std::vector<int> mandatoryIndices;
     dictionary.AddMandatoryWords("mandatory", HEIGHT, mandatoryIndices);
 
@@ -378,8 +372,7 @@ int main(void)
 
     if(mandatoryIndices.size())
     {
-        size_t wordCount = 4 // Borders
-                         + 2*(WIDTH+HEIGHT-4); // 2 words per col/row
+        const size_t wordCount = 2*(WIDTH+HEIGHT); // 2 words per col/row
         if(mandatoryIndices.size() > wordCount)
         {
             // TODO Error
@@ -393,12 +386,12 @@ int main(void)
 
         std::vector<std::thread> pool;
         for(size_t i = 0; i < 4; ++i)
-            pool.emplace_back(run_concurrently, indices, 4, i);
+            pool.emplace_back(run_concurrently, FANCY_BORDERS, indices, 4, i);
         for(auto &thread : pool)
             thread.join();
     }
     else
-        run_single(4);
+        run_single(4, FANCY_BORDERS);
 
     delete dfa_borderH;
     delete dfa_borderV;
